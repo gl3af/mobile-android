4b31b47926d81408f4d2a83be02635c7b7a70c0a
Nikita <ybaranovnikita2002@gmail.com>
2023-10-01 21:05:27 +0300

    Card

--- Unit-1/Projects/Quadrant/app/src/main/java/com/example/quadrant/MainActivity.kt
+++ Unit-1/Projects/Quadrant/app/src/main/java/com/example/quadrant/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.quadrant
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.quadrant.ui.theme.QuadrantTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Card()
                 }
             }
         }
@@ -29,17 +39,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+private fun Card(modifier: Modifier = Modifier) {
+    val title = stringResource(R.string.first_title)
+    val description = stringResource(R.string.first_text)
+    val backgroundColor = Color(0xFFEADDFF)
+
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = title,
+            modifier = Modifier.padding(bottom = 16.dp),
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = description,
+            textAlign = TextAlign.Justify
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     QuadrantTheme {
-        Greeting("Android")
+        Card()
     }
 }
