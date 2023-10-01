dc71ed8f50f73150ddd5ab3757c21215fa5b8d5b
Nikita <ybaranovnikita2002@gmail.com>
2023-10-01 21:55:19 +0300

    Card

--- Unit-2/Projects/ClickBehavior/app/src/main/java/com/example/clickbehavior/MainActivity.kt
+++ Unit-2/Projects/ClickBehavior/app/src/main/java/com/example/clickbehavior/MainActivity.kt
@@ -3,13 +3,26 @@ package com.example.clickbehavior
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.clickbehavior.ui.theme.ClickBehaviorTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +30,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ClickBehaviorTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Card()
                 }
             }
         }
@@ -30,17 +42,42 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun Card(modifier: Modifier = Modifier) {
+    val text = R.string.lemon_select
+    val image = R.drawable.lemon_tree
+    val description = R.string.lemon_tree_content_description
+
+    Box(
         modifier = modifier
-    )
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Button(onClick = {}) {
+                Image(
+                    painter = painterResource(image),
+                    contentDescription = stringResource(description),
+                    modifier = Modifier
+                        .width(128.dp)
+                        .height(160.dp)
+                        .padding(24.dp)
+                )
+            }
+            Spacer(modifier = Modifier.height(32.dp))
+            Text(
+                text = stringResource(text),
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ClickBehaviorPreview() {
     ClickBehaviorTheme {
-        Greeting("Android")
+        Card()
     }
 }
