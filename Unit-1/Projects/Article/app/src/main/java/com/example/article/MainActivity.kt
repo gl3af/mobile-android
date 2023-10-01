bb690c80eaa1e1e576d214de7257f30aa74a5557
Nikita <ybaranovnikita2002@gmail.com>
2023-10-01 20:21:46 +0300

    Text layout

--- Unit-1/Projects/Article/app/src/main/java/com/example/article/MainActivity.kt
+++ Unit-1/Projects/Article/app/src/main/java/com/example/article/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.article
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.article.ui.theme.ArticleTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ArticleApp()
                 }
             }
         }
@@ -29,17 +38,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ArticleApp(modifier: Modifier = Modifier) {
+    val title = stringResource(R.string.title)
+    val shortDescription = stringResource(R.string.short_text)
+    val longDescription = stringResource(R.string.long_text)
+
+    Column(modifier = modifier) {
+        Text(
+            text = title,
+            modifier = Modifier.padding(16.dp),
+            fontSize = 24.sp
+        )
+        Text(
+            text = shortDescription,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = longDescription,
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ArticleAppPreview() {
     ArticleTheme {
-        Greeting("Android")
+        ArticleApp()
     }
 }
